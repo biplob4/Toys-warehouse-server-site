9664f4e48df8c2a27ad538f39331a0f2e35ca858
biplob4 <biplobhossen6020@gmail.com>
2022-05-01 04:30:23 +0600

    problem

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+
+app.get('/',(req,res)=>{
+    res.send('server is Raddy')
+})
+app.listen(port,()=>{
+    console.log('server is Ranning');
+})
