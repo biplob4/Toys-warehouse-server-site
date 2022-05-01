7437f6c0647798945b3c906c5fd1bdd8ebee4529
biplob4 <biplobhossen6020@gmail.com>
2022-05-01 07:58:09 +0600

    hosting heroku

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ app.use(express.json());
 require('dotenv').config();
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { query } = require('express');
+const uri = "mongodb+srv://toysdb:AlsW4MWbX4OcIz5K@cluster0.bjlw9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const toysCollection = client.db("toysCollection").collection("toys");
+
+async function run (){
+    try{
+        await client.connect();
+
+        app.get('/toysLimited',async(req,res)=>{
+            const query = {};
+            const cursor = toysCollection.find(query);
+            const regult = await cursor.limit(6).toArray();
+            res.send(regult);
+        })
+
+    }finally{}
+}
+run().catch(console.dir);
+
+
+
+
 app.get('/',(req,res)=>{
     res.send('server is Raddy')
 })
