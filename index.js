65f491a7a819c9abcc72bdbdfa38f3f3dc98ecdf
biplob4 <biplobhossen6020@gmail.com>
2022-05-02 11:20:23 +0600

    secres pass

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 
 const { query } = require('express');
-const uri = "mongodb+srv://toysdb:AlsW4MWbX4OcIz5K@cluster0.bjlw9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://toysdb:${process.env.REACR_PASS}@cluster0.bjlw9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const toysCollection = client.db("toysCollection").collection("toys");
 
