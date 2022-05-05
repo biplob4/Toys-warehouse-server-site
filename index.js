3e0ae019cb04130641cb980a42ba8e35452baac9
biplob4 <biplobhossen6020@gmail.com>
2022-05-05 22:18:41 +0600

    problem check

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 require('dotenv').config();
 
